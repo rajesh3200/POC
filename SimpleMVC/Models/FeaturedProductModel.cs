065688058ae7b092c96695b664251180d7038816
Rajesh kumar <s.rajesh3200.feeds@gmail.com>
2014-05-14 18:11:04 +0530

    improved the code

---
+++ SimpleMVC/Models/FeaturedProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleMVC.Models
+{
+    public class FeaturedProduct
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
